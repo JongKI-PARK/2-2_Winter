32b4d0305bbd944c17bcc442885af5cd9c8d7021
JongKI-PARK <97037853+JongKI-PARK@users.noreply.github.com>
2022-02-01 14:38:01 +0900

    Add files via upload
    using binary search method

---
+++ 2주(1월 30일)/bj2805_bs.cpp
@@ -0,0 +1,43 @@
+//bj2805_bs.cpp 2805 using binary search 
+//2022.2.1 KI
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int count, length;
+	int array[1000]={0};
+	scanf_s("%d %d", &count, &length);
+
+	if (length <= 0) {
+		printf("No cutting is needed\n");
+		exit(0);
+	}
+
+	int max = 0;
+	for (int i = 0; i < count; i++) {
+		scanf_s("%2d", &array[i]);
+		if (max < array[i])
+			max = array[i];
+	}
+	int sum=0;
+	int mid = max / 2;
+	int min = 0;
+	while (sum != length) {
+		sum = 0;
+		for (int i = 0; i < count; i++) {
+			if (array[i] > mid)
+				sum += array[i] - mid;
+		}
+		if (sum > length) {
+			min = mid;
+			mid = (max + min) / 2;
+		}
+		else if (sum < length) {
+			max = mid;
+			mid = (max + min) / 2;
+		}
+	}
+	printf("%d\n", mid);
+	return 0;
+}
